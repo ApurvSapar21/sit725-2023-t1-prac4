727a7a68aa86db20d6d8c9cd8915c920aee5720f
ApurvSapar21 <128551701+ApurvSapar21@users.noreply.github.com>
2023-08-07 20:55:59 +1000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -13,17 +13,25 @@ const cardList = [
     }
 ]
 
+const getProjects = () => {
+    $.get('/api/projects', (response) => {
+        if (response.statusCode == 200) {
+            addCards(response.data);
+        }
+    })
+}
+
 const addCards = (items) => {
     items.forEach(item => {
-        let itemToAppend = '<div class="col s4 center-align">'+
-    '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
-    '</div><div class="card-content">'+
-    '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
-    '<div class="card-reveal">'+
-        '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
-        '<p class="card-text card-desc-color">'+item.desciption+'</p>'+
-      '</div></div></div>';
-      $("#card-section").append(itemToAppend)
+        let itemToAppend = '<div class="col s4 center-align">' +
+            '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + item.image + '">' +
+            '</div><div class="card-content">' +
+            '<span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#">' + item.link + '</a></p></div>' +
+            '<div class="card-reveal">' +
+            '<span class="card-title grey-text text-darken-4">' + item.title + '<i class="material-icons right">close</i></span>' +
+            '<p class="card-text card-desc-color">' + item.desciption + '</p>' +
+            '</div></div></div>';
+        $("#card-section").append(itemToAppend)
     });
 }
 
@@ -41,11 +49,11 @@ const submitForm = () => {
     console.log("Form Data Submitted: ", formData);
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.materialboxed').materialbox();
-    addCards(cardList);
-    $('.modal').modal();
-    $('#formSubmit').click(()=>{
+    $('#formSubmit').click(() => {
         submitForm();
     })
-})
+    getProjects();
+    $('.modal').modal();
+});
