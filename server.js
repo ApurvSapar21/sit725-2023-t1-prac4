771b215d0f013c21ab753f30f4dac325b4a05781
ApurvSapar21 <128551701+ApurvSapar21@users.noreply.github.com>
2023-08-07 20:55:31 +1000

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-const express = require("express")
-const app = express()
-
-app.use(express.static(__dirname+'/public'))
+var express = require("express")
+var app = express()
+app.use(express.static(__dirname + '/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-const port = process.env.port || 3000;
-
-app.listen(port,()=>{
-    console.log("App running at http://localhost:"+port)
+const cardList = [
+    {
+        title: "Kitten 2",
+        image: "images/kitten-2.jpg",
+        link: "About Kitten 2",
+        desciption: "Demo desciption about kitten 2"
+    },
+    {
+        title: "Kitten 3",
+        image: "images/kitten-3.jpg",
+        link: "About Kitten 3",
+        desciption: "Demo desciption about kitten 3"
+    }
+]
+app.get('/api/projects', (req, res) => {
+    res.json({ statusCode: 200, data: cardList, message: "Success" })
+})
+var port = process.env.port || 3000;
+app.listen(port, () => {
+    console.log("App listening to: " + port)
 })
